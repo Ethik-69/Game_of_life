b31520a2781ad2f72ea0349bdd82e84b1d617a1c
TheSystem <ethancmk@hotmail.fr>
2015-05-11 16:00:12 +0200

    maj

--- site_flask/hello.py
+++ site_flask/hello.py
@@ -4,15 +4,14 @@
 from StringIO import StringIO
 from flask import render_template
 from datetime import date
-from flask import Flask, request
+from flask import Flask, request, redirect
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     mots = ["bonjour", "a", "toi,", "visiteur."]
     da = date.today().isoformat()
-    return render_template('index.html', titre="Bienvenue !", mots=mots, date=da)
+    return render_template('corps_index.html', titre="Bienvenue !", mots=mots, date=da)
 
 
 @app.route('/test/')
@@ -22,9 +21,16 @@ def test():
         return "Je suis : {} - {}".format(nom, prenom) + ' nous sommes ici : ' + request.path
 
 
-@app.route('/contact/')
+@app.route('/contact/', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html', titre="Contact")
+    if request.method == 'GET':
+       return render_template('contact.html', titre="Contact")
+    else:
+        return render_template('contact.html', titre="Contact")
+
+#@app.route('/contact/')
+#def contact():
+#    return render_template('contact.html', titre="Contact")
 
 
 if __name__ == '__main__':
