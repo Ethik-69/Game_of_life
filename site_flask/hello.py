3e3473d5a1d92208c2c8cb916a4f7699cc762d4c
TheSystem <ethancmk@hotmail.fr>
2015-05-22 11:06:35 +0200

    todo

--- site_flask/hello.py
+++ site_flask/hello.py
@@ -1,11 +1,46 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
-#from PIL import image
-from StringIO import StringIO
-from flask import render_template
+import os
+import json
+import argparse
+import rethinkdb as rdb
 from datetime import date
-from flask import Flask, request, redirect
+from werkzeug import secure_filename
+from rethinkdb.errors import RqlRuntimeError, RqlDriverError
+from flask import Flask, request, redirect, flash, render_template
+from flask import Flask, g, jsonify, render_template, request, abort
+
+RDB_HOST =  os.environ.get('RDB_HOST') or 'localhost'
+RDB_PORT = os.environ.get('RDB_PORT') or 28015
+TODO_DB = 'todoapp'
+
+def dbSetup():
+    connection = rdb.connect(host=RDB_HOST, port=RDB_PORT)
+    try:
+        rdb.db_create(TODO_DB).run(connection)
+        rdb.db(TODO_DB).table_create('todos').run(connection)
+        print 'Database setup completed. Now run the app without --setup.'
+    except RqlRuntimeError:
+        print 'App database already exists. Run the app without --setup.'
+    finally:
+        connection.close()
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.before_request
+def before_request():
+    try:
+        g.rdb_conn = rdb.connect(host=RDB_HOST, port=RDB_PORT, db=TODO_DB)
+    except RqlDriverError:
+        abort(503, "No database connection could be established.")
+
+@app.teardown_request
+def teardown_request(exception):
+    try:
+        g.rdb_conn.close()
+    except AttributeError:
+        pass
 
 @app.route('/')
 def index():
@@ -14,24 +49,28 @@ def index():
     return render_template('corps_index.html', titre="Bienvenue !", mots=mots, date=da)
 
 
-@app.route('/test/')
-def test():
-        nom = "CHAMIK"
-        prenom = "Ethan"
-        return "Je suis : {} - {}".format(nom, prenom) + ' nous sommes ici : ' + request.path
+def requet(task):
+    flash('{}'.format(task))
+    rdb.db('todoapp').table('todos').insert([{ 'name' : '{}'.format(task) }])
 
 
-@app.route('/contact/', methods=['GET', 'POST'])
-def contact():
+
+@app.route('/todo/', methods=['GET', 'POST'])
+def todo():
     if request.method == 'GET':
-       return render_template('corps_contact.html', titre="Contact")
+        flash('ToDo :')
     else:
-        return render_template('corps_contact.html', titre="Contact")
+        if request.method == 'POST':
+            requet(request.form['task'])
 
-#@app.route('/contact/')
-#def contact():
-#    return render_template('corps_contact.html', titre="Contact")
+    selection = list(rdb.table('todos').run(g.rdb_conn))
+    return render_template('corps_todo.html', titre="Contact", selec=selection)
+
+
+
 
 
 if __name__ == '__main__':
+    app.secret_key = '\xf8\xff\xbc\xfe\xde\x03\x8b\x81\xc9\x9c\xc4\xbe\x95\xa2\xf2'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug=True)
